ceebfb082340434f4495b6d21416760c5a9cc915
rafestevam <rafestevam@gmail.com>
2020-04-15 19:13:42 -0300

    heroku deploy adj

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/NOME_DO_SEU_PROJETO'));
+app.use(express.static(__dirname + '/dist/fc-ms-front-material'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/NOME_DO_SEU_PROJETO/index.html'));
+res.sendFile(path.join(__dirname+'/dist/fc-ms-front-material/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
